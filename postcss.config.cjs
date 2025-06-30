1f6f8392378a1dc2e58101dbd9f20699a53ba0d4
rajnisht7 <rajnishtiwari9787@gmail.com>
2025-06-30 12:29:35 +0530

    update postcss

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,6 +1,6 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: [
+    require('@tailwindcss/postcss')(), // ✅ correct plugin
+    require('autoprefixer'),
+  ],
 };
